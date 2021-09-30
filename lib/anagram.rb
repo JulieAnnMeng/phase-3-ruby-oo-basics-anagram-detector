07cef96399c21860c273861ff7a446e4d6f24a5d
Julie Meng <julieannmeng@gmail.com>
2021-09-30 14:16:34 -0400

    commit tree 665d9ec339235acd0348df4e907e464a3e785727

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def word
+        @word
+    end
+
+    def match array
+        array.filter {|item| item.chars.sort == self.word.chars.sort}
+    end
+end
